bc53e16ff5cedd4e50c54c5e7dacb0e53ef01348
Luke Na <narukeu@outlook.com>
2026-08-16 15:04:45 +0800

    feat: 架构

---
+++ oxfmt.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'oxfmt';
+
+export default defineConfig({
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 120,
+  tabWidth: 2,
+  endOfLine: 'lf',
+  semi: true,
+});
